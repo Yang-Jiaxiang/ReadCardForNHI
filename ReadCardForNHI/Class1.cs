21e2deeb44cc85a7fb4c1fe2fd66b83dc8297bfc
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-01-18 06:12:02 +0800

    first commit

---
+++ ReadCardForNHI/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ReadCardForNHI
+{
+    public class Startup
+    {
+        public struct SCARD_IO_REQUEST
+        {
+            public int dwProtocol;
+            public int cbPciLength;
+        }
+
+        //引用 PC/SC(Personal Computer/Smart Card) API WinScard.dll
+        [DllImport("WinScard.dll")]
+        public static extern int SCardEstablishContext(uint dwScope,
+          int nNotUsed1, int nNotUsed2, ref int phContext);
+        [DllImport("WinScard.dll")] public static extern int SCardReleaseContext(int phContext);
+        [DllImport("WinScard.dll")]
+        public static extern int SCardConnect(int hContext, string cReaderName,
+          uint dwShareMode, uint dwPrefProtocol, ref int phCard, ref int ActiveProtocol);
+        [DllImport("WinScard.dll")] public static extern int SCardDisconnect(int hCard, int Disposition);
+        [DllImport("WinScard.dll")]
+        public static extern int SCardListReaders(int hContext, string cGroups,
+          ref string cReaderLists, ref int nReaderCount);
+        [DllImport("WinScard.dll")]
+        public static extern int SCardTransmit(int hCard,
+          ref SCARD_IO_REQUEST pioSendPci, byte[] pbSendBuffer, int cbSendLength,
+          ref SCARD_IO_REQUEST pioRecvPci, ref byte pbRecvBuffer, ref int pcbRecvLength);
+
+        public async Task<object> Invoke()
+        {
+            return this.getData();
+        }
+        
+        public string getData()
+        {
+            string Data = "CC";
+            int ContextHandle = 0, CardHandle = 0, ActiveProtocol = 0, ReaderCount = -1;
+            string ReaderList = string.Empty; //讀卡機名稱列表
+            SCARD_IO_REQUEST SendPci, RecvPci;
+            byte[] SelectAPDU = { 0x00, 0xA4, 0x04, 0x00, 0x10, 0xD1, 0x58, 0x00, 0x00, 0x01, 0x00, 0x00,0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11, 0x00 };
+            byte[] ReadProfileAPDU = { 0x00, 0xca, 0x11, 0x00, 0x02, 0x00, 0x00 };
+            byte[] SelectRecvBytes = new byte[2]; //應回 90 00
+            int SelectRecvLength = 2;
+            byte[] ProfileRecvBytes = new byte[59]; //接收Profile的 Byte Array
+            int ProfileRecvLength = 59;
+
+            //建立 Smart Card API
+            if (SCardEstablishContext(0, 0, 0, ref ContextHandle) == 0)
+                //列出可用的 Smart Card 讀卡機
+                if (SCardListReaders(ContextHandle, null, ref ReaderList, ref ReaderCount) == 0)
+                    //建立 Smart Card 連線
+                    if (SCardConnect(ContextHandle, ReaderList, 1, 2, ref CardHandle, ref ActiveProtocol) == 0)
+                    {
+                        SendPci.dwProtocol = RecvPci.dwProtocol = ActiveProtocol;
+                        SendPci.cbPciLength = RecvPci.cbPciLength = 8;
+                        //下達 Select Profile 檔的 APDU
+                        if (SCardTransmit(CardHandle, ref SendPci, SelectAPDU, SelectAPDU.Length,
+                          ref RecvPci, ref SelectRecvBytes[0], ref SelectRecvLength) == 0)
+                        //下達讀取Profile指令
+                        {
+                            if (SCardTransmit(CardHandle, ref SendPci, ReadProfileAPDU, ReadProfileAPDU.Length,
+                              ref RecvPci, ref ProfileRecvBytes[0], ref ProfileRecvLength) == 0)
+                                Console.WriteLine(
+                                     @"健保卡ID:{0}
+                                     姓名:{1}
+                                     身份証字號:{2}
+                                     生日:{3}/{4}/{5}
+                                     姓別:{6}
+                                     發卡日期:{7}/{8}/{9}",
+                                      Encoding.Default.GetString(ProfileRecvBytes, 0, 12),
+                                      Encoding.Default.GetString(ProfileRecvBytes, 12, 6),
+                                      Encoding.Default.GetString(ProfileRecvBytes, 32, 10),
+                                      Encoding.Default.GetString(ProfileRecvBytes, 43, 2),
+                                      Encoding.Default.GetString(ProfileRecvBytes, 45, 2),
+                                      Encoding.Default.GetString(ProfileRecvBytes, 47, 2),
+                                      Encoding.Default.GetString(ProfileRecvBytes, 49, 1),
+                                      Encoding.Default.GetString(ProfileRecvBytes, 51, 2),
+                                      Encoding.Default.GetString(ProfileRecvBytes, 53, 2),
+                                      Encoding.Default.GetString(ProfileRecvBytes, 55, 2)
+                                  );
+                                Data = Encoding.Default.GetString(ProfileRecvBytes, 0, 12)+
+                                      Encoding.Default.GetString(ProfileRecvBytes, 12, 6)+
+                                      Encoding.Default.GetString(ProfileRecvBytes, 32, 10)+
+                                      Encoding.Default.GetString(ProfileRecvBytes, 43, 2)+
+                                      Encoding.Default.GetString(ProfileRecvBytes, 45, 2)+
+                                      Encoding.Default.GetString(ProfileRecvBytes, 47, 2)+
+                                      Encoding.Default.GetString(ProfileRecvBytes, 49, 1)+
+                                      Encoding.Default.GetString(ProfileRecvBytes, 51, 2)+
+                                      Encoding.Default.GetString(ProfileRecvBytes, 53, 2)+
+                                      Encoding.Default.GetString(ProfileRecvBytes, 55, 2);
+                        }
+                    }
+            return Data;
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ReadCardForNHI;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 x = new Class1();
+            string data = x.getData();
+            label1.Text = data;
+        }
+    }
+}
